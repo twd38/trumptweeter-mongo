5c54841f38cbef8789fe80e1416713b3ff088138
Will Davis <twd38@cornell.edu>
2018-10-09 17:15:40 -0400

    download functionality added

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const downloadCsv = require('download-csv')
 const server = require('express')
 const mongo = require('mongodb')
 const keys = require('./config/keys')
+const json2csv = require('json2csv').parse;
+const fs = require('fs')
 
 const express = server()
 const db = mongo.MongoClient
+
+var searchResults = []
 
 express.set('view engine', 'ejs')
 express.use(server.static(__dirname+'/views'))
@@ -40,8 +45,53 @@ db.connect(keys.mongoURI, (err,client) => {
     var cursor = database.collection('things').find({"Tweet": {$regex : ".*"+req.query.searchString+".*"}}).sort( sortBy ).toArray( function(err,results){
       console.log(sortBy)
       res.json({status:'Success', message:'found tweet', tweet:results});
+      searchResults = results
+      // searchString = searchString.push(res.query.searchString)
     })
   })
+
+  express.get('/export', (req,res) => {
+    console.log("Export to CSV");
+
+    var json = searchResults;
+    var jsonLength  = searchResults.length;
+    var columns= ['_id','','Agency','Date','Job','Name','Tweet','Twitter_Handle','tweet_URL'];
+    const opts = { columns };
+    console.log(json);
+    // var json = JSON.parse(json_pre);
+
+    // const items = json
+    // const replacer = (key, value) => value === null ? '' : value // specify how you want to handle null values here
+    // const header = Object.keys(items[0])
+    // let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+    // csv.unshift(header.join(','))
+    // csv = csv.join('\r\n')
+    //
+    // console.log(csv)
+    //
+    // downloadCsv(csv, columns, "trumptweeter-export.csv");
+    // // downloadFile(csv, "trumptweeter-export.csv");
+
+    try {
+      const csv = json2csv(json, opts);
+      console.log(csv);
+      fs.writeFile('output.csv',csv, (err) => {
+          if (err) {
+              console.log(err);
+          }
+          else {
+              console.log('wrote output.csv');
+              res.download('output.csv');
+          }
+      });
+
+    } catch (err) {
+      console.error(err);
+    }
+
+  })
+
+
 })
 
 const PORT = process.env.PORT || 3000;
