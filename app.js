f632bdd2eedd3d74e38ee55b833ba90adb146e9a
Will Davis <twd38@cornell.edu>
2018-10-09 22:20:04 -0400

    gitignore keys

--- app.js
+++ app.js
@@ -58,19 +58,6 @@ db.connect(keys.mongoURI, (err,client) => {
     var columns= ['_id','','Agency','Date','Job','Name','Tweet','Twitter_Handle','tweet_URL'];
     const opts = { columns };
     console.log(json);
-    // var json = JSON.parse(json_pre);
-
-    // const items = json
-    // const replacer = (key, value) => value === null ? '' : value // specify how you want to handle null values here
-    // const header = Object.keys(items[0])
-    // let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-    // csv.unshift(header.join(','))
-    // csv = csv.join('\r\n')
-    //
-    // console.log(csv)
-    //
-    // downloadCsv(csv, columns, "trumptweeter-export.csv");
-    // // downloadFile(csv, "trumptweeter-export.csv");
 
     try {
       const csv = json2csv(json, opts);
